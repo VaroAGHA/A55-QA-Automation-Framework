4d39d609adaceab6e7f3b4ad75729e0adc107606
VaroAGHA <varazdat.aghabekyan@testpro.io>
2024-02-13 14:24:46 -0500

    Homework-23
    Corrected Mistakes for HW22 as well

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePage extends BasePage {
@@ -12,30 +13,52 @@ public class HomePage extends BasePage {
     }
 
     //Web-Elements
-
     String newPlaylistName = "Sample Edited Playlist";
-            By userAvatarIcon = By.cssSelector("img.avatar");
+    //getUserAvatar
+    By userAvatarIcon = By.cssSelector("img.avatar");
 
+    //chooseAllSongsList
+    By allSongsList = By.cssSelector("Li a.songs");
+
+    //doubleClickPlaylist
+    By playlistElementLocator = By.cssSelector(".playlist:nth-child(6)");
+
+    //enterNewName
+    @FindBy(css = "[name='name']" )
+    By playlistInputFieldLocator = By.cssSelector("[name='name']");
+
+
+    //notification
+    By notificationLocator = By.cssSelector("div.success.show");
+
+    /*-----------------------------------------------------------------------------------------*/
     //Helper Methods
     public WebElement getUserAvatar() {
         return findElement(userAvatarIcon);
+    }
 
+    public void chooseAllSongsList() {
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("Li a.songs"))).click();
+        findElement(allSongsList).click();
     }
 
     public void doubleClickPlaylist() {
-        WebElement playlistElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(6)")));
+         //WebElement playlistElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(6)")));
+        WebElement playlistElement =findElement(playlistElementLocator);
         actions.doubleClick(playlistElement).perform();
     }
 
     public void enterNewName() {
-        WebElement playlistInputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
+        WebElement playlistInputField = findElement(playlistInputFieldLocator);
+        //WebElement playlistInputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
         playlistInputField.sendKeys(Keys.chord(Keys.CONTROL, "A", Keys.BACK_SPACE));
         playlistInputField.sendKeys(newPlaylistName);
         playlistInputField.sendKeys(Keys.ENTER);
     }
 
     public String getRenamePlaylistSuccessMsg() {
-        WebElement notification = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
+        //WebElement notification = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
+        WebElement notification = findElement(notificationLocator);
         return notification.getText();
     }
 }
--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -2,34 +2,44 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
 
-public class LoginPage extends BasePage{
+/**
+ * This is class is using Selenium By Abstract class to locate elements.
+ * Login Helper Methods are using By class for locators.
+ */
+
+public class LoginPage extends BasePage {
 
     public LoginPage(WebDriver givenDriver) {
         super(givenDriver);
     }
-//Web Elements
-    By emailField = By.cssSelector("input[type='email']");
-By passwordField = By.cssSelector("input[type='password']");
-By loginBtn = By.cssSelector("button[type='submit']");
 
-//Helper Methods
-    public void provideEmail(String email){
+    //Web Elements
+    @FindBy(id = "email")
+    By emailField = By.cssSelector("input[type='email']");
+    @FindBy(id = "password")
+    By passwordField = By.cssSelector("input[type='password']");
+    @FindBy(id="submit")
+    By loginBtn = By.cssSelector("button[type='submit']");
+
+    //Helper Methods
+    public void provideEmail(String email) {
         findElement(emailField).sendKeys(email);
     }
 
-   public void providePassword(String password){
+    public void providePassword(String password) {
 
         findElement(passwordField).sendKeys(password);
-   }
+    }
 
-   public void clickSubmit(){
+    public void clickSubmit() {
         findElement(loginBtn).click();
-   }
-   public void login(){
+    }
+    public void login() {
         provideEmail("varag@testpro.io");
         providePassword("te$t$tudent1");
         clickSubmit();
-   }
-
+    }
 }
+
